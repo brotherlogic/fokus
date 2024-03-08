a358c19e5e2f0c6f94ef194b52020ee406c82b5c
Simon Tucker <brotherlogic@gmail.com>
2024-03-07 16:54:23 -0800

    Basic structure

---
+++ server/api.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/brotherlogic/fokus/proto"
+)
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) GetFokus(ctx context.Context, req *pb.GetFokusRequest) (*pb.GetFokusResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "Not ready")
+}
