ef1495d7e554ba6c77ef6f87dc44f1f92c620fca
Simon <brotherlogic@gmail.com>
2025-02-25 06:30:42 -0800

    Also allow 'Blocked on'

--- server/api.go
+++ server/api.go
@@ -78,7 +78,7 @@ func (s *Server) trimToActionable(ctx context.Context, issues []*ghbpb.GithubIss
 			return comments.Comments[i].GetTimestamp() > comments.Comments[j].GetTimestamp()
 		})
 
-		if len(comments.Comments) > 0 && strings.HasPrefix(comments.Comments[0].GetText(), "Block on") {
+		if len(comments.Comments) > 0 && (strings.HasPrefix(comments.Comments[0].GetText(), "Block on") || strings.HasPrefix(comments.Comments[0].GetText(), "Blocked on")) {
 			elems := strings.Split(strings.Fields(comments.Comments[0].GetText())[2], "/")
 			number, err := strconv.ParseInt(elems[2], 10, 32)
 			if err != nil {
